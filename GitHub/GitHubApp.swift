7a3d121f06365e98e037b97b4b9d43db8ab89f9e
Karla Castro <castromorenokarla0@gmail.com>
2023-06-12 11:49:37 -0400

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by scholar on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
